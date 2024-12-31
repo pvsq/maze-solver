85f41fc4866cf11f3e577ba93e9b0fd25e61a092
pvsq <pv_sqra@protonmail.com>
2024-12-31 12:54:05 +0530

    Test adding cells with various wall configurations

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from cell import Cell
 from line import Line
 from point import Point
 from window import Window
@@ -5,11 +6,25 @@ from window import Window
 def main():
     win = Window(800, 600)
 
-    # Test drawing some lines
-    win.draw_line(Line(Point(50, 20), Point(90, 100)), "black")
-    win.draw_line(Line(Point(150, 20), Point(450, 600)), "red")
-    win.draw_line(Line(Point(0,0), Point(800,600)), "green")
-    win.draw_line(Line(Point(800,0), Point(0,600)), "yellow")
+    # Test drawing cells with walls
+    c1 = Cell(50, 100, 100, 150, win,
+              has_left_wall=True, has_top_wall=False,
+              has_right_wall=False, has_bottom_wall=False)
+    c1.draw()
+    c2 = Cell(100, 150, 150, 200, win)
+    c2.draw()
+    c3 = Cell(150, 200, 200, 250, win,
+              has_left_wall=False, has_top_wall=True,
+              has_right_wall=False, has_bottom_wall=True)
+    c3.draw()
+    c4 = Cell(200, 250, 250, 300, win,
+              has_left_wall=False, has_top_wall=False,
+              has_right_wall=True, has_bottom_wall=True)
+    c4.draw()
+    c5 = Cell(250, 300, 300, 350, win,
+              has_left_wall=True, has_top_wall=True,
+              has_right_wall=False, has_bottom_wall=False)
+    c5.draw()
 
     win.wait_for_close()
 
