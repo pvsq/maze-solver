c759f5074ed2c33e36e1d2fc5818aba184cd22b8
pvsq <pv_sqra@protonmail.com>
2024-12-31 13:23:16 +0530

    Test draw_move in main.py

--- main.py
+++ main.py
@@ -6,25 +6,23 @@ from window import Window
 def main():
     win = Window(800, 600)
 
-    # Test drawing cells with walls
+    # Test drawing cells and moves from one cell to another
     c1 = Cell(50, 100, 100, 150, win,
-              has_left_wall=True, has_top_wall=False,
-              has_right_wall=False, has_bottom_wall=False)
-    c1.draw()
-    c2 = Cell(100, 150, 150, 200, win)
-    c2.draw()
-    c3 = Cell(150, 200, 200, 250, win,
-              has_left_wall=False, has_top_wall=True,
-              has_right_wall=False, has_bottom_wall=True)
-    c3.draw()
-    c4 = Cell(200, 250, 250, 300, win,
-              has_left_wall=False, has_top_wall=False,
-              has_right_wall=True, has_bottom_wall=True)
-    c4.draw()
-    c5 = Cell(250, 300, 300, 350, win,
               has_left_wall=True, has_top_wall=True,
               has_right_wall=False, has_bottom_wall=False)
-    c5.draw()
+    c1.draw()
+    c2 = Cell(50, 100, 400, 450, win,
+              has_left_wall=True, has_top_wall=False,
+              has_right_wall=True, has_bottom_wall=True)
+    c2.draw()
+    c3 = Cell(500, 550, 100, 150, win,
+              has_left_wall=False, has_top_wall=True,
+              has_right_wall=True, has_bottom_wall=True)
+    c3.draw()
+
+    c1.draw_move(c2, undo=False)
+    c3.draw_move(c1, undo=True)
+    c2.draw_move(c3, undo=False)
 
     win.wait_for_close()
 
