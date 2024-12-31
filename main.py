34f06d2420dbcab031d83a5735347f6e0b0dc498
pvsq <pv_sqra@protonmail.com>
2024-12-31 11:28:24 +0530

    Test drawing a few lines on window

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+from line import Line
+from point import Point
 from window import Window
 
 def main():
     win = Window(800, 600)
+
+    # Test drawing some lines
+    win.draw_line(Line(Point(50, 20), Point(90, 100)), "black")
+    win.draw_line(Line(Point(150, 20), Point(450, 600)), "red")
+    win.draw_line(Line(Point(0,0), Point(800,600)), "green")
+    win.draw_line(Line(Point(800,0), Point(0,600)), "yellow")
+
     win.wait_for_close()
 
 
