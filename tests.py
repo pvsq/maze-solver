2130b3f61b1953fbd9a6551101e5f99683f6272f
pvsq <pv_sqra@protonmail.com>
2025-01-12 23:32:30 +0530

    Add test for cells after the maze is created. Fix test for zero rows and columns

--- tests.py
+++ tests.py
@@ -20,13 +20,10 @@ class Tests(unittest.TestCase):
     def test_maze_with_no_cols_or_rows(self):
         num_cols = 0
         num_rows = 0
-        m1 = Maze(50, 50, num_rows, num_cols, 75, 75)
-        self.assertEqual(
-            len(m1._cells),
-            num_cols,
-        )
+        # It shouldn't be possible to create a maze with
+        # zero rows and zero columns.
         with self.assertRaises(IndexError):
-            m1._cells[0]
+            Maze(50, 50, num_rows, num_cols, 75, 75)
 
 
     def test_maze_with_one_cell(self):
@@ -61,6 +58,19 @@ class Tests(unittest.TestCase):
             m1._cells[m1.num_cols-1][m1.num_rows-1].has_bottom_wall,
             False,
         )
+
+
+    def test_maze_reset_cells_visited(self):
+        num_cols = 14
+        num_rows = 12
+        m1 = Maze(50, 50, num_rows, num_cols, 50, 50, seed=37431)
+        visited_count = 0
+        actual_count = 0
+        for i in range(num_cols):
+            for j in range(num_rows):
+                if m1._cells[i][j].visited == True:
+                    actual_count += 1
+        self.assertEqual(visited_count, actual_count)
 
 
 
